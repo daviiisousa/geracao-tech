9b79c41702994b49b6b63b9eebba09400707b874
daviiisousa <sousadavi248@gmail.com>
2024-10-02 09:14:55 -0300

    if else

---
+++ Javascript/estrutura/condicionais/app.js
@@ -0,0 +1,29 @@
+// if e else
+if (idade >= 18) {
+  console.log("vc e maior de idade");
+} else {
+  console.log("vc e menor de idade");
+}
+
+// essa estrutura de repetição verifica se a pessoa e maior ou menor de idade
+
+// operador ternario
+
+idade >= 18 ? "vc e maior de idade" : "vc e menor de idade"; // essa e uma forma abreviada de fazer um if else
+
+// switch
+
+switch (categoriasCliente) {
+  case "regular":
+    console.log("desconto 5%");
+    break;
+  case "vip":
+    console.log("desconto = 20%");
+    break;
+  default:
+    console.log("desconto = 0%");
+}
+ let categoriasCliente = "regular"
+
+ // um if else com mais opçao e mais robusto 
+ 
