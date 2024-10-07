bd0aa3e00be6bad4c917dc36654b842d8d9a39b0
daviiisousa <sousadavi248@gmail.com>
2024-10-07 11:20:56 -0300

    comecamos a ver funçoes

---
+++ Javascript/funcoes/funcoes.js
@@ -0,0 +1,16 @@
+function saudacao(nome){
+    console.log(`ola ${nome} tudo bom`)
+}
+
+saudacao("davi")
+
+function soma(){
+    a = 5
+    b = 5
+    console.log(a + b)
+}
+
+soma()
+
+//funçoes sao blocos com algoritmos dentro 
+//funçoes podem receber parametros que funcionam como variaveis que essa funçao vai receber 
