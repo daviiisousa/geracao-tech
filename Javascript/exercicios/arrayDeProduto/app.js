ae61986f714e53c48c04a471854205548889479d
daviiisousa <sousadavi248@gmail.com>
2024-10-08 19:04:54 -0300

    array de produtos

--- Javascript/exercicios/arrayDeProduto/app.js
+++ Javascript/exercicios/arrayDeProduto/app.js
@@ -3,56 +3,51 @@
 // preco (número)
 // quantidade (número)
 
-const produtos = [
-  
-];
-
 // Uma função para adicionar um novo produto.
 // Uma função para remover um produto com base no id.
 // Uma função para atualizar a quantidade de um produto.
 // Uma função para listar todos os produtos no inventário.
 
+const produtos = [
+];
+
 const form = document.getElementById("form");
+const resposta = document.getElementById("resposta");
+const inputProduto = document.getElementById("produtos");
+const inputPreco = document.getElementById("preco");
+const inputQuantidade = document.getElementById("qtd");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const inputProduto = document.getElementById("produtos").value;
-  const inputPreco = document.getElementById("preco").value;
-  const inputQuantidade = document.getElementById("qtd").value;
-  const resposta = document.getElementById("resposta");
+  const nome = inputProduto.value;
+  const preco = Number(inputPreco.value); 
+  const qtd = Number(inputQuantidade.value); 
 
-  resposta.innerHTML = `
-  produto: ${inputProduto}
-  <br>
-  quantidade: ${inputQuantidade}
-  <br>
-  preço: ${inputPreco}
-  `;
+  addNovoProduto(nome, preco, qtd);
 
+  console.log(produtos)
 
-  addNovoProduto(inputProduto);
-  addNovoPreco(inputPreco);
-  addNovoQuantidade(inputQuantidade);
-  
+  inputProduto.value = "";
+  inputPreco.value = "";
+  inputQuantidade.value = "";
 });
 
-function addNovoProduto(produto) {
-    for(let i in produtos){
-        produtos[i].nome = produto;
-    }
+function addNovoProduto(nome, preco, qtd) {
+  const produto = {
+    nome: nome,
+    preco: preco,
+    quantidade: qtd,
+  };
+
+  resposta.innerHTML += `Produto: ${nome} <br> Preço: ${preco} <br> Quantidade: ${qtd} <br><br>`
+  produtos.push(produto)
 }
 
-function addNovoPreco(preco) {
-    for(let i in produtos){
-        produtos[i].preco = preco;
-    }
-}
 
-function addNovoQuantidade(qtd) {
-    for(let i in produtos){
-        produtos[i].quantidade = qtd;
-    }
-}
 
-console.log(produtos);
+
+
+
+
+
