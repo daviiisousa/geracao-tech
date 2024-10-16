b53bdbda983b8bd938487f90db9f48c7ebd8ddc5
daviiisousa <sousadavi248@gmail.com>
2024-10-16 13:17:47 -0300

    links

--- React/Components/src/components/nav.jsx
+++ React/Components/src/components/nav.jsx
@@ -4,9 +4,9 @@ export function Nav(){
             <nav className="flex justify-between px-10 py-3 items-center">
                 <h1 className="text-3xl">Components</h1>
                 <ul className="flex gap-5 ">
-                    <li>Home</li>
-                    <li>Sobre</li>
-                    <li>Redes</li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Redes</a></li>
                 </ul>
             </nav>
         </>
