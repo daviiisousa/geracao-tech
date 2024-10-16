e17bd055e3a88c46d7910552904398647b39fbd6
daviiisousa <sousadavi248@gmail.com>
2024-10-16 14:33:40 -0300

    estilos

--- React/Components/src/components/form.jsx
+++ React/Components/src/components/form.jsx
@@ -2,7 +2,7 @@ import { BtnEnviar } from "./buttons/btnEnviar";
 
 export function Form({children}){
     return(
-        <form className="mx-20 p-3 bg-zinc-950 rounded-md" action="#">
+        <form className="m-20 p-3 bg-zinc-950 rounded-md" action="#">
             <h1 className="text-2xl text-center mb-3">Cadastro</h1>
             {children}
             <BtnEnviar />
--- React/Components/src/components/headear.jsx
+++ React/Components/src/components/headear.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export function Headear({children}){
     return(
         <>
-            <header className="bg-blue-400 w-screen ">
+            <header className="bg-blue-400 w-[100%] ">
                 {children}
             </header>
         </>
