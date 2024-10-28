bb9891c6c4589ce5137517f5d86a42d6d91228a1
daviiisousa <sousadavi248@gmail.com>
2024-10-28 09:43:29 -0300

    crud

--- Node/Api-express/controller/userController.js
+++ Node/Api-express/controller/userController.js
@@ -16,33 +16,52 @@ export const getAllUsers = (_req, res) => {
 };
 
 export const getUserById = (req, res) => {
-  const userId = parseInt(req.params.id)
-  
+  const userId = parseInt(req.params.id);
+
   const user = users.find((u) => {
-    return u.id == userId
-  })
-  
-  if(!user){
-    const error = new Error('usuario não cadastrado')
-    error.statusCode = 404
-    res.send("Usuário não encontrado")
+    return u.id == userId;
+  });
+
+  if (!user) {
+    const error = new Error("usuario não cadastrado");
+    error.statusCode = 404;
+    res.send("Usuário não encontrado");
   }
-  res.json(user)
-}
+  res.json(user);
+};
 
 export const createUser = (req, res) => {
-  const {nome, email} = req.body
-  if(!nome || !email){
-    return res
-    .status(400)
-    .json({error: 'Nome e email sao obrigatorios'})
+  const { nome, email } = req.body;
+  if (!nome || !email) {
+    return res.status(400).json({ error: "Nome e email sao obrigatorios" });
   }
   const newUser = {
     id: users.length + 1,
     nome,
-    email
-  }
-  users.push(newUser)
-  res.status(201).json(newUser)
-}
+    email,
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
 
+export const updateUser = (req, res) => {
+  const userId = parseInt(req.params.id)
+  const {nome, email} = req.body
+
+  const userIndex = users.findIndex((u) => u.id == userId)
+  if(userIndex === -1){
+    return res.status(404).json({error: 'usuario nao encontrado'})
+  }
+  if(nome) users[userIndex].nome = nome
+  if(email) users[userIndex].email = email
+
+  res.json(users[userIndex])
+};
+
+export const deleteUser = (req, res) => {
+  const userId = parseInt(req.params.id)
+  users = users.find((u) => {
+    return u.id !== userId
+  })
+  res.json({message: 'deu certo'})
+}
--- Node/Api-express/routes/users.js
+++ Node/Api-express/routes/users.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import {createUser, getAllUsers, getUserById} from '../controller/userController.js'
+import {createUser, deleteUser, getAllUsers, getUserById, updateUser} from '../controller/userController.js'
 export const router = express.Router()
 
 router.get('/', getAllUsers)
 router.get('/:id', getUserById)
 router.post('/', createUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
